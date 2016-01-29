64c97eea38e3bd60cee697b6b5578318f46f2b80
Martin Dybdal <dybber@dybber.dk>
2016-01-29 17:03:55 +0100

    Fix error message in mclFinish

--- mcl.c
+++ mcl.c
@@ -483,7 +483,7 @@ void mclFinish(mclContext ctx) {
   cl_int status = clFinish(ctx.command_queue);
   MCL_VALIDATE(status, "Error on mclFinish (blocks till cmd queue is empty)");
   if(status != CL_SUCCESS) {
-    printf("ErrorErrorErrorErrorErrorError\n");
+    fprintf(stderr, "mclFinish: %s\n", mclErrorToString(ret));
   }
 }
 
